dc69fb62fde624949cc4ac5a34fe5a8ee7a3487d
MrSixtorz <sixto723@gmail.com>
2023-11-07 19:35:16 +0100

    Solucionado problemas con compileSdk y con la imagen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.tablelayout_01"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tablelayout_01"
